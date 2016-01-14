4ef38c5746d077088ca5a0967587795eccd49368
djig <jig_nash@yahoo.com>
2016-01-13 16:38:56 -0800

    test
    Sync

---
+++ TableBar/FirstNode.js
@@ -0,0 +1,69 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1343\cocoasubrtf160
+{\fonttbl\f0\fnil\fcharset0 Monaco;}
+{\colortbl;\red255\green255\blue255;\red200\green208\blue172;\red36\green36\blue36;\red255\green255\blue255;
+\red144\green191\blue97;}
+\margl1440\margr1440\vieww28300\viewh15000\viewkind0
+\deftab720
+\pard\pardeftab720\sl440
+
+\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 var http \cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 =\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2  require\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 (\cf5 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec5 'http'\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 );\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \
+http\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 .\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 createServer\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 (\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 function \cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 (\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 req\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 ,\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2  res\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 )\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2  \cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 \{\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \
+  res\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 .\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 writeHead\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 (\cf5 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec5 200\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 ,\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2  \cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 \{\cf5 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec5 'Content-Type'\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 :\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2  \cf5 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec5 'text/plain'\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 \});\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \
+  res\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 .\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 end\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 (\cf5 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec5 'Hello World\\n'\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 );\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \
+\pard\pardeftab720\sl440
+\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 \}).\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 listen\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 (\cf5 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec5 1337\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 ,\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2  \cf5 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec5 '127.0.0.1'\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 );\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \
+console\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 .\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 log\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 (\cf5 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec5 'Server running at http://127.0.0.1:1337/'\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 );\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \
+}
--- TableBar/JS/bt.js
+++ TableBar/JS/bt.js
@@ -10,9 +10,50 @@
 	 function TableBar(element, options) {
 	 	this.element = $(element);
 		this.options =  options || {};
- 		this._name = pluginName;
+ 		this.name = pluginName;
+ 		this.init();
  		 console.log(options); 
 	}
+
+	TableBar.prototype = {
+		defaults: {
+			theme: 'dark',
+			pos: 'right'
+		},
+		init: function() {
+
+			this.config = $.extend({}, this.defaults, this.options);
+		
+		},
+		
+		populateData: function() {
+			var isURL = this.isURL;
+
+			var showHide = '<div id="qn_sh"><span>Show/Hide</span></div>';
+			var divNotes = '<div id="notes"></div>';
+			var notesInp = '<p><input type="text" name="qn_input" maxlength="500" placeholder="Your notes..."></p>';
+			$(showHide).appendTo(this.$el);
+			$(divNotes).appendTo(this.$el);
+			$(notesInp).appendTo(this.$el.find('#notes'));
+
+		
+		},
+		hook: function() {
+		
+			this.$el.on('keypress', '#notes input', function(e) {
+			});
+
+			// SHOW AND HIDE
+			this.$el.on('click', '#qn_sh span', function() {
+				$('.qn_container #notes').slideToggle(100);
+			});
+
+			this.$el.on('click', '#notes .close', function() {
+				});
+		}
+	};
+
+
 	$.fn[pluginName] = function (method)  {
 		 
 	 	return this.each(function () {
